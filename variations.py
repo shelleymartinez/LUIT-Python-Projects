932b3383de96c9e71bd36513a0d46e4016dbd0ae
shelleymartinez <shelley.martinez@gmail.com>
2022-06-12 01:31:54 +0000

    Added a new filenamed variations.py and added code to file to-celsius.py

---
+++ variations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7
+
+# Python implementation here
+
+message = input("Enter a message: ")
+
+print("Lowercase:", message.lower())
+print("Uppercase:", message.upper())
+print("Capitalized:", message.capitalize())
+print("Title Case:", message.title())
+
+words = message.split()
+print("Words:", words)
+
+sorted_words = sorted(words)
+print("Alphabetic First Word:", sorted_words[0])
+print("Alphabetic Las Word:", sorted_words[-1])
