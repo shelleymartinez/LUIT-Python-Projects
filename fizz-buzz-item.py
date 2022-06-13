a4a85ecf1c606e5e495350dadbeed77ebd1f38cb
shelleymartinez <shelley.martinez@gmail.com>
2022-06-13 23:18:16 +0000

    Added to the file fizz-buzz-item.py

--- fizz-buzz-item.py
+++ fizz-buzz-item.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python3.7
+
 # Python implementation goes here
 
-value = input("Enter an integer value: ")
+value = int(input("Enter an integer value: "))
 
+if value % 5 == 0 and value % 3 == 0:
+    print("FizzBuzz")
+elif value % 3 == 0:
+    print("Fizz")
+elif value % 5 == 0:
+    print("Buzz")
+else:
+    print(value)
