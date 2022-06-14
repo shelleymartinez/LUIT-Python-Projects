fb6edf329c58ef2ac8e26b2eba04030d5c00bb7f
shelleymartinez <shelley.martinez@gmail.com>
2022-06-14 00:17:26 +0000

    Adding a new file named if-else.py

---
+++ if-else.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.7
+
+n = int(input())
+
+if n % 2 == 0 and n == 2>5:
+    print("Not Weird")
+
+elif n % 2 == 0 and n == 6>20:
+    print("Weird")
+    
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")
+
+else:
+    print("Weird")
