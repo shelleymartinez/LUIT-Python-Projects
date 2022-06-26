26e1dddda68b04cc0fddb48e3d1dfe7d2f18bf44
shelleymartinez <shelley.martinez@gmail.com>
2022-06-26 09:40:30 +0000

    Added a new file named Project_16.py

---
+++ Project_16.py
@@ -0,0 +1,38 @@
+# A python script that you can run that will stop 3 instances
+
+import boto3
+ec2_resource=boto3.resource("ec2")
+ec2_client=boto3.client("ec2")
+
+# create 3 instances
+ec2_resource.create_instances(
+    ImageId='ami-0d9858aa3c6322f73',
+    InstanceType='t2.micro',
+    MaxCount=6,
+    MinCount=3)
+    
+# list instance ids
+response = ec2_client = boto3.describe_instances()
+instances = response['Reservations']
+instance_ids = []
+
+for instance in instances:
+    instance_ids.append(instance['Instances'] [0] ['InstanceID'])
+
+# tag 3 instances for Development team
+tagged_instances = [instance_ids[:3]]
+tag_creation= ec2_client.Tag('resource_id','key','value')
+tag_creation = ec2_client.create_tags(
+    Resources =
+        tagged_instances,
+    Tags = [
+        {
+            'Key':'Environment',
+            'Value': 'Dev'
+        }
+    ]
+)
+
+# stop instances
+instances = [tagged_instances]
+ec2_client.instances.filter(InstanceIds=instances).stop()
