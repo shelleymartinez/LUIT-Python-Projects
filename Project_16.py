1c1f602bb1896384958629449f49a5829f006fdb
shelleymartinez <shelley.martinez@gmail.com>
2022-06-26 10:10:33 +0000

    modified file Project_16.py

--- Project_16.py
+++ Project_16.py
@@ -20,7 +20,7 @@ for instance in instances:
     instance_ids.append(instance['Instances'] [0] ['InstanceId'])
 
 # tag 3 instances for Development team
-tagged_instances = (instance_ids[:3])
+tagged_instances = (instance_ids[1:4])
 tag_creation = ec2_client.create_tags(
     Resources =
         tagged_instances,
@@ -33,5 +33,4 @@ tag_creation = ec2_client.create_tags(
 )
 
 # stop instances
-instances = tagged_instances
-ec2_client.instances.filter(InstanceIds=instances).stop()
+response = ec2_client.stop_instances(InstanceIds=tagged_instances)
