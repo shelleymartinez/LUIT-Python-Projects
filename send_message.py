d78af8bacfb030ecec16e36cf9f2221ba8a495e3
shelleymartinez <shelley.martinez@gmail.com>
2022-07-03 06:50:15 +0000

    Added new file named send_message.py and modified lambda_function_send_message.py

--- send_message.py
+++ send_message.py
@@ -1,4 +1,4 @@
-# lambda function to send sqs
+# send a message to sqs queue
 import logging
 import boto3
 from botocore.exceptions import ClientError
@@ -12,16 +12,15 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
 sqs_client = boto3.client("sqs", region_name=AWS_REGION)
-queue_url = 'https://sqs.us-west-1.amazonaws.com/789034859404/standard_sqs'
 
-def lambda_handler(event, context):
-    
+def send_message():
     sqs_client = boto3.client("sqs", region_name="us-west-1")
 
     message = {"key": "value"}
     response = sqs_client.send_message(
-        QueueUrl="https://sqs.us-west-1.amazonaws.com/789034859404/standard_sqs",
+        QueueUrl="https://us-west-2.queue.amazonaws.com/xxx/my-new-queue",
         MessageBody=json.dumps(message)
     )
     print(response)
+   
    
